c0798855887c024a4ef5da5895c7355b9dcb9588
MrTuyennn <nguyenngoctuyen188@gmail.com>
2022-01-10 11:41:53 +0700

    add font && add hermesjs && add babel-plugin-module-resolver -- babel-plugin-transform-remove-console

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@apis': './src/apis',
+          '@assets': './src/assets',
+          '@fonts': './src/assets/fonts',
+          '@images': './src/assets/images',
+          '@icons': './src/assets/icons',
+          '@svgs': './src/assets/svgs',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@screens': './src/screens',
+          '@state': './src/state',
+          '@navigation': './src/navigation',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    // 'react-native-reanimated/plugin',
+  ],
 };
